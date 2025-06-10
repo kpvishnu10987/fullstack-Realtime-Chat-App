f45c1b26e3ce3f7902ae2e5b4698d138a737ed0a
vishnu <kpvishnu10987@gmail.com>
2025-06-10 14:16:46 +0530

    Express Version Downgraded

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,14 +26,17 @@ app.use(express.json());
 
 
 
-
+console.log("Registering /api/auth");
 app.use("/api/auth", authRoutes);
+
+console.log("Registering /api/messages");
 app.use("/api/messages", messageRoutes);
 
+console.log("Registering wildcard route");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
