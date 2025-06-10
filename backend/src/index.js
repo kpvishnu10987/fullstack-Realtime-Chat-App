8cc91d13ebb59b398d06edb4d36959286dd797a5
vishnu <kpvishnu10987@gmail.com>
2025-06-10 14:24:37 +0530

    index.js in backend corrected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,48 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js"
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
+
 import path from "path";
-import { app ,server} from "./lib/socket.js";
 
+import { connectDB } from "./lib/db.js";
 
-dotenv.config(); // Load environment variables
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-
-
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true,
-}));
-app.use(cookieParser());
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-
-
-console.log("Registering /api/auth");
 app.use("/api/auth", authRoutes);
-
-console.log("Registering /api/messages");
 app.use("/api/messages", messageRoutes);
 
-console.log("Registering wildcard route");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-
 server.listen(PORT, () => {
-  console.log("Server is running at port:", PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
